40308623d255af26eb45b01bf44a89c3239cae3d
ajonesb <alanjones83@gmail.com>
2021-01-03 18:57:56 -0500

    feature / CommitListComponent - Styles and functionality to show recent commits

--- src/CommitListComponent.js
+++ src/CommitListComponent.js
@@ -4,7 +4,7 @@ function CommitListComponent() {
   const [commits, setCommits] = useState([]);
 
   useEffect(() => {
-    fetch("https://api.github.com/repos/ajonesb/react-js-movies/commits")
+    fetch("https://api.github.com/repos/ajonesb/react-github-commits/commits")
       .then((response) => response.json())
       .then((data) => {
         setCommits(data);
@@ -14,19 +14,21 @@ function CommitListComponent() {
   return (
     <div className="container-fluid">
       <div className="row">
+        <h1>Most Recent Commits</h1>
         {commits.map((commit) => (
           <div className="col-md-4 card" key={commit.node_id}>
-            <span className="card-body">
-              <h2 className="card-title">Node ID:</h2>
-              <p className="card-text">{commit.sha}</p>
-            </span>
             <span className="card-body">
               <h2 className="card-title">Commit Message:</h2>
               <p className="card-text"> {commit.commit.message}</p>
             </span>
             <span className="card-body">
-              <h2 className="card-title">Commit URL:</h2>
-              <p className="card-text">{commit.commit.url}</p>
+              <a
+                className="btn btn-primary"
+                href={commit.html_url}
+                role="button"
+              >
+                Visit Commit on Github
+              </a>
             </span>
           </div>
         ))}
